a0eed6773b97d5748c9c3ba980b8c8aa35e2fd8b
Usamahafiz8 <usamahafiz8@gmail.com>
2023-11-27 15:20:06 +0500

    working on the USDT transfer <SuccessFull>

--- controllers/transectionControler/usdtTransferControler.js
+++ controllers/transectionControler/usdtTransferControler.js
@@ -1,11 +1,15 @@
-const usdtABI = require('../../contractAbi/USDT.json'); 
-const USDTAddress = process.env.USDT_CONTRACT_ADDRESS;  
+const ethers = require("ethers");
+require("dotenv").config();
+const infuraApiKey = process.env.Infura_Private_Key;
+const usdtABI = require("../../contractAbi/USDT.json");
+const USDTAddress = process.env.USDT_CONTRACT_ADDRESS;
 
 class USDTTransferController {
   constructor(privateKey, recipientAddress, amountToSend) {
     this.privateKey = privateKey;
     this.recipientAddress = recipientAddress;
-    this.amountToSend = amountToSend;
+    this.amountToSend = ethers.utils.parseUnits("0.001", 6); // 6 is the number of decimals for USDT
+
 
     this.infuraUrl = `https://polygon-mainnet.infura.io/v3/${infuraApiKey}`;
     this.provider = new ethers.providers.JsonRpcProvider(this.infuraUrl);
@@ -16,45 +20,68 @@ class USDTTransferController {
   async checkBalance() {
     try {
       const balance = await this.tokenContract.balanceOf(this.wallet.address);
-      console.log("USDT Token Balance:", ethers.utils.formatUnits(balance, 'wei'));
+      console.log(
+        "USDT Token Balance:",
+        ethers.utils.formatUnits(balance, "wei")
+      );
     } catch (error) {
       console.error("Error checking balance:", error.message);
     }
   }
-
   async transferTokensAPI(req, res) {
     try {
       const nonce = await this.wallet.getTransactionCount();
       const gasPrices = await this.wallet.getGasPrice();
-
+  
       console.log("Gas Prices (Wei):", gasPrices.toString());
-
+  
       // Adjust the gas price calculation dynamically (increase by 10%)
       const gasPrice = gasPrices.add(gasPrices.div(10));
-
+  
       console.log("Adjusted Gas Price:", gasPrice.toString());
-
-      const usdtBalance = await this.tokenContract.balanceOf(this.wallet.address);
+  
+      const usdtBalance = await this.tokenContract.balanceOf(
+        this.wallet.address
+      );
       if (usdtBalance.lt(this.amountToSend)) {
-        throw new Error("Insufficient USDT funds for the transaction. Please add more USDT tokens.");
+        throw new Error(
+          "Insufficient USDT funds for the transaction. Please add more USDT tokens."
+        );
       }
-
-      const tx = await this.tokenContract.transfer(this.recipientAddress, this.amountToSend, {
+  
+      const usdtInterface = new ethers.utils.Interface(usdtABI);
+      const transferData = usdtInterface.encodeFunctionData('transfer', [this.recipientAddress, this.amountToSend]);
+  
+      const tx = await this.wallet.sendTransaction({
+        to: USDTAddress,
+        data: transferData,
         gasPrice: gasPrice,
         nonce: nonce,
       });
-
+  
       await tx.wait();
       console.log("Transaction Hash:", tx.hash);
-
+  
       await this.checkBalance();
-
-      res.json({ success: true, message: 'Tokens transferred successfully.', TransactionHash: `${tx.hash}` });
+  
+      res.json({
+        success: true,
+        message: "Tokens transferred successfully.",
+        TransactionHash: `${tx.hash}`,
+      });
     } catch (error) {
       console.error("Error transferring USDT tokens:", error.message);
-      res.status(500).json({ success: false, message: 'Error transferring tokens.', error: error.message });
+      res
+        .status(500)
+        .json({
+          success: false,
+          message: "Error transferring tokens.",
+          error: error.message,
+        });
     }
   }
+  
+  
 }
 
 module.exports = USDTTransferController;
