ee27bef47256fdc356dab81053266bb466f30620
Usamahafiz8 <usamahafiz8@gmail.com>
2023-11-28 15:08:48 +0500

    modifed the API according to the need

--- controllers/transectionHistoryControler.js
+++ controllers/transectionHistoryControler.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const getTransactionsList = async (walletAddress, res) => {
   try {
-    const apiKey = process.env.POLYGONSCAN_API_KEY; // You need to set up an account on Polygon Scan and get your API key
+    const apiKey = process.env.POLYGONSCAN_API_KEY;
     const apiUrl = `https://api.polygonscan.com/api`;
     const module = 'account';
     const action = 'txlist';
@@ -28,20 +28,49 @@ const getTransactionsList = async (walletAddress, res) => {
       });
     }
 
-    const transactions = response.data.result.slice(0, 10); // Only take the first 10 transactions
+    const transactions = response.data.result.slice(0, 10);
 
-    // Format the transaction data as needed
-    const formattedTransactions = transactions.map(tx => ({
-      hash: tx.hash,
-      from: tx.from,
-      to: tx.to,
-      value: ethers.utils.formatUnits(tx.value, 'wei'), // Adjust unit as needed
-      timestamp: new Date(tx.timeStamp * 1000), // Convert timestamp to Date object
-    }));
+    const formattedTransactions = transactions.map(async (tx) => {
+      const txDetails = await axios.get(apiUrl, {
+        params: {
+          module: 'transaction',
+          action: 'getstatus',
+          txhash: tx.hash,
+          apikey: apiKey,
+        },
+      });
+    
+      const status = txDetails.data.result.isError === '0' ? 'Success' : 'Failure';
+    
+      const gasUsed = ethers.BigNumber.from(tx.gasUsed);
+      const gasPrice = ethers.BigNumber.from(tx.gasPrice);
+    
+      // Calculate fees in wei
+      const feesWei = gasUsed.mul(gasPrice);
+    
+      // Convert fees to MATIC (1 MATIC = 10^18 wei)
+      const feesMatic = ethers.utils.formatUnits(feesWei, 'ether');
+    
+      return {
+        hash: tx.hash,
+        from: tx.from,
+        to: tx.to,
+        value: ethers.utils.formatUnits(tx.value, 'wei'),
+        timestamp: new Date(tx.timeStamp * 1000),
+        status: status,
+        senderAddress: tx.from,
+        receiverAddress: tx.to,
+        fees: feesMatic, // Fees in Matic
+      };
+    });
+    
+    const transactionsWithDetails = await Promise.all(formattedTransactions);
+    
+    
 
-    res.json({ success: true, transactionsHistory: formattedTransactions });
+    res.json({ success: true, transactionsHistory: transactionsWithDetails });
   } catch (error) {
-    console.error("Error fetching transactionsHistory:", error.message);
+    console.error('Error fetching transactionsHistory:', error.message);
     res.status(500).json({ success: false, message: 'Error fetching transactionsHistory.', error: error.message });
   }
 };
