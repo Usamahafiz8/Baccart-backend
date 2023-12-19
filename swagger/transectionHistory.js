38ef40e61d0ddf7e6af041179d15ae9d7ba24cc3
Usamahafiz8 <usamahafiz8@gmail.com>
2023-12-19 17:24:52 +0500

    readjust the swagers code

--- swagger/transectionHistory.js
+++ swagger/transectionHistory.js
@@ -1,8 +1,3 @@
-const express = require('express');
-const { getTransactionsList } = require('../controllers/transectionHistoryControler'); // Import the correct function
-
-const router = express.Router();
-
 /**
  * @swagger
  * tags:
@@ -31,19 +26,3 @@ const router = express.Router();
  *       500:
  *         description: Internal server error.
  */
-router.get('/:walletAddress', async (req, res) => {
-  const { walletAddress } = req.params;
-
-  if (!walletAddress) {
-    return res.status(400).json({ success: false, message: 'Wallet address is required.' });
-  }
-
-  try {
-    await getTransactionsList(walletAddress, res); // Use the correct function name
-  } catch (error) {
-    console.error("Error fetching Polygon transactionsHistory:", error.message);
-    res.status(500).json({ success: false, message: 'Error fetching Polygon transactionsHistory.', error: error.message });
-  }
-});
-
-module.exports = router;
